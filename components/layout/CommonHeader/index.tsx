ede13946cf130aef8ad7522705e309112ce5df59
FHL <yezhixingyue@163.com>
2021-02-03 18:44:29 +0800

    2.3

--- components/layout/CommonHeader/index.tsx
+++ components/layout/CommonHeader/index.tsx
@@ -72,6 +72,7 @@ export default function index() {
       offsetTop: _offsetTop,
     })
     window.addEventListener('scroll', handleListenScroll);
+    handleListenScroll();
     return () => {
       window.removeEventListener('scroll',handleListenScroll);
     }
