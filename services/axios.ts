06396d9aba6b13dcbd974ed947bed7e89757528f
FHL <yezhixingyue@163.com>
2021-04-20 09:07:13 +0800

    4.20-1

--- services/axios.ts
+++ services/axios.ts
@@ -26,15 +26,15 @@ axios.interceptors.request.use(
 axios.interceptors.response.use(
   (response) => {
     const _list2NotNeed2Toast = ['/Api/ExpressWaybill/Record/Excel', '/api/Customer/Login'];
-    const _statusList2NotNeed2Toast = [1000, 7025];
+    const _statusList2NotNeed2Toast = [1000, 7025, 8037];
     // 包含以上的状态码 或 以上的请求路径  不会弹窗报错  其余以外都会报错出来
 
     const _url = response.config && response.config.url ? response.config.url.split('?')[0] : '';
 
-    if ([7025, 8037].includes(response.data.Status)) {
-      message.error(response.data.Message);
-      return response;
-    }
+    // if ([7025, 8037].includes(response.data.Status)) {
+    //   message.error(response.data.Message);
+    //   return response;
+    // }
     if (!_statusList2NotNeed2Toast.includes(response.data.Status) && !_list2NotNeed2Toast.includes(_url) && (!closeTip) && isBrower()) {
       const _obj: {
         title?: string | undefined;
